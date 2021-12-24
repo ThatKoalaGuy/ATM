26235b02eea36c53816e1f8322b097ed0c18fbbb
Domen Koder <domen.koder@gmail.com>
2021-12-24 19:13:16 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+string Balance = "1000 EUR";
+
+Console.WriteLine("What is your name? ");
+string userName = Console.ReadLine();
+Console.WriteLine("You are: " + userName);
+Console.WriteLine("");
+Console.WriteLine(userName + ", your balance is " + Balance);
+
+Console.WriteLine("What do you want your new balance to be? ");
+string newBalance = Console.ReadLine() + " EUR";
+Console.WriteLine("Your new balance is " + newBalance);
+
+
+
+
+
+
+
+
+
