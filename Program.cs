f035afe98788cd944e7b27fe4aab7b211da6f5e6
Domen Koder <domen.koder@gmail.com>
2021-12-24 23:27:32 +0100

    Deposit not working

--- Program.cs
+++ Program.cs
@@ -1,17 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
-float Balance = 1000;
 
 Console.WriteLine("What is your name? ");
 string userName = Console.ReadLine();
 Console.WriteLine("You are: " + userName);
-Console.WriteLine("");
-Console.WriteLine(userName + ", your balance is " + Balance + " EUR");
+Console.WriteLine("How much money do you have? ");
+string Balance = Console.ReadLine();
+float startBalance = float.Parse(Balance);
 
-Console.WriteLine("How much money do you wish to withdraw? ");
-string sBalance = Console.ReadLine();
-float newBalance = float.Parse(sBalance);
+Console.WriteLine(userName + ", your balance is " + startBalance + " EUR");
+Console.WriteLine("Press 1 for WITHDRAWAL");
+Console.WriteLine("Press 2 for DEPOSIT");
 
-Console.WriteLine("Your new balance is " + (Balance - newBalance) + " EUR");
+
+if (Console.ReadKey().Key == ConsoleKey.NumPad1)
+{
+    Console.ReadLine();
+    Console.WriteLine("How much money do you wish to withdraw? ");
+    string Withdrawal = Console.ReadLine();
+    float wBalance = float.Parse(Withdrawal);
+
+    Console.WriteLine("Your new balance is " + (startBalance - wBalance) + " EUR");
+    Console.ReadLine();
+    Environment.Exit(0);
+
+} 
+
+
+
+if (Console.ReadKey().Key == ConsoleKey.NumPad2)
+{
+    Console.ReadLine();
+    Console.WriteLine("How much money do you wish to deposit? ");
+    string Deposit = Console.ReadLine();
+    float dBalance = float.Parse(Deposit);
+
+    Console.WriteLine("Your new balance is " + (startBalance - dBalance) + " EUR");
+    Console.ReadLine();
+    Environment.Exit(0);
+
+}
+
+else Console.WriteLine("There was no choice for this number");
+
 
 
 
